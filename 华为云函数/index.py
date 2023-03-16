beae50c956d3e29c4a6f048ce36781fba41f9f95
LinChiQ <111717457+LinChiQ@users.noreply.github.com>
2023-03-16 16:34:02 +0800

    Update index.py

--- 华为云函数/index.py
+++ 华为云函数/index.py
@@ -1,6 +1,9 @@
 from WoZaiXiaoYuan import Start
 from GoOut import go_out
+from time import localtime
 
 def handler(event , context):
-    Start()
-    go_out()
+    if localtime().tm_hour >= 20:
+        Start()
+    else:
+        go_out()
