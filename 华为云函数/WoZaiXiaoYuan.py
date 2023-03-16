514277ba6e839186fc2ac54f14049b070d54103c
LinChiQ <111717457+LinChiQ@users.noreply.github.com>
2023-03-16 16:27:09 +0800

    Update WoZaiXiaoYuan.py

--- 华为云函数/WoZaiXiaoYuan.py
+++ 华为云函数/WoZaiXiaoYuan.py
@@ -2,6 +2,7 @@ import requests
 import json
 import yagmail
 import yaml
+import time
 
 
 def Login(headers, username, password):
@@ -129,6 +130,67 @@ def GetConfigs():
     return configs
 
 
+def DelayBackTime(id , headers):
+    delay_url = 'https://gw.wozaixiaoyuan.com/out/mobile/out/saveDelay?' + id
+    delay_json = {"endDate":f"{time.localtime().tm_hour + 1}:00","delayContent":"事"}
+    req = requests.post(delay_url, json=delay_json, headers=headers)
+    text = json.loads(req.text)
+    if text['code'] == 0:
+        print("延期成功！")
+        return True
+    else:
+        print("延期失败！" , text)
+        return False
+
+
+def BackToSchool(headers_one , jws):
+    get_one_url = 'https://gw.wozaixiaoyuan.com/out/mobile/out/getOne'
+    req = requests.get(get_one_url, headers=headers_one)
+    get_one = json.loads(req.text)
+    id = get_one['data']['id']
+    date = get_one['data']['end'].split(' ')[0].split('-')
+    state = int(get_one['data']['state'])
+    if time.localtime().tm_mday != int(date[-1]) or time.localtime().tm_mon != int(date[-2]) or time.localtime().tm_year!= int(date[0]):
+        print("已超过一天逾期！")
+        return False
+    if state == 4:
+        if not DelayBackTime(id , headers_one):
+            return False
+    if state == 5:
+        print("已手动返校！")
+        return True
+    req = requests.get(get_one_url, headers=headers_one)
+    get_second = json.loads(req.text)
+    state = get_second['data']['state']
+    id = get_second['data']['id']
+    headers_two = {
+        'Host': 'gw.wozaixiaoyuan.com',
+        'accept': 'application/json, text/plain, */*',
+        'jwsession': jws,
+        'user-agent': 'Mozilla/5.0 (Linux; Android 8.1.0; OPPO R11st Build/OPM1.171019.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4425 MMWEBSDK/20230202 Mobile Safari/537.36 MMWEBID/4162 MicroMessenger/8.0.33.2320(0x28002137) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wxce6d08f781975d91',
+        'content-type': 'application/json;charset=UTF-8',
+        'x-requested-with': 'com.tencent.mm',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': f'https://gw.wozaixiaoyuan.com/h5/mobile/out/index/out/back?id={id}',
+        'accept-encoding': 'gzip, deflate',
+        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cookie': f'JWSESSION={jws}',
+        'cookie': f'JWSESSION={jws}'
+    }
+    if state == 2:
+        back_url = 'https://gw.wozaixiaoyuan.com/out/mobile/out/back?id=' + id
+        req = requests.get(back_url, headers=headers_two)
+        text = json.loads(req.text)
+        if text['code'] == 0:
+            print("成功返校！")
+            return True
+        else:
+            print("返校失败！", text)
+            return False
+
+
 def main():
     for config in configs:
         username = config['username']
@@ -158,6 +220,12 @@ def main():
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
         }
+        if config['back']:
+            if BackToSchool(headers , jws):
+                print(username , "返校成功！")
+                mail.send(config['receive'] , '返校成功！' , '返校成功！')
+            else:
+                mail.send(config['receive'] , '返校失败！' , '返校失败！请查看！')
         batch = GetUnDo(headers, username)
         if not batch:
             continue
