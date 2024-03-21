cd05c3a88d9f55391e92d1eab92e021677b39ab0
Lin <111717457+LinChiQ@users.noreply.github.com>
2024-03-21 22:25:21 +0800

    更新登录以及对应的cookie

--- 服务器/WoZaiXiaoYuan.py
+++ 服务器/WoZaiXiaoYuan.py
@@ -41,22 +41,25 @@ def Login(headers, username, password):
         "password": encrypted_text
     }
     login_req = requests.post(login_url, params=params, headers=headers)
+    print(login_req.text)
     text = json.loads(login_req.text)
     if text['code'] == 0:
         print(f"{username}账号登陆成功！")
         set_cookie = login_req.headers['Set-Cookie']
-        jws = re.search(r'JWSESSION=.*?;', str(set_cookie)).group(0)
-        cookie = jws
-        return cookie
+        jws = re.search(r'JWSESSION=(.*?);', str(set_cookie)).group(1)
+        wzxySession = re.search(r'WZXYSESSION=(.*?);', str(set_cookie)).group(1)
+        return jws, wzxySession
     else:
         print(f"{username}登陆失败，请检查账号密码！")
         return False
 
-def testLoginStatus(headers, cookie):
+def testLoginStatus(headers, jws, wzxySession):
     # 用任意需要鉴权的接口即可，这里随便选了一个
     url = "https://gw.wozaixiaoyuan.com/health/mobile/health/getBatch"
     headers['Host'] = "gw.wozaixiaoyuan.com"
-    headers['Cookie'] = cookie
+    headers['cookie'] = f'JWSESSION={jws}'
+    headers['cookie'] = f'JWSESSION={jws}'
+    headers['cookie'] = f'WZXYSESSION={wzxySession}'
     res = requests.get(url, headers=headers)
     text = json.loads(res.text)
     if text['code'] == 0:
@@ -121,15 +124,17 @@ def GetLocation(config_locations):
 
 def GetEachJws(config, headers):
     try:
-        return jws_data[config['username']]
+        jws = jws_data[config['username']].split(',')[0]
+        wzxySession = jws_data[config['username']].split(',')[1]
+        return jws, wzxySession
     except:
         print(config['username'], "尝试登陆！")
-        cookie = Login(headers, config['username'], config['password'])
-        if cookie is False:
+        jws, wzxySession = Login(headers, config['username'], config['password'])
+        if jws is False:
             mail.send(config['receive'], '登陆失败', '登陆失败，请检查账号密码')
             return False
-        jws_data[str(config['username'])] = cookie
-        return cookie
+        jws_data[str(config['username'])] = f"{jws},{wzxySession}"
+        return jws, wzxySession
 
 
 def Punch(headers, batch, answers, receive, username):
@@ -218,75 +223,31 @@ def DelayBackTime(id , headers):
         return False
 
 
-def BackToSchool(headers_one , cookie):
-    get_one_url = 'https://gw.wozaixiaoyuan.com/out/mobile/out/getOne'
-    req = requests.get(get_one_url, headers=headers_one)
-    get_one = json.loads(req.text)
-    id = get_one['data']['id']
-    date = get_one['data']['end'].split(' ')[0].split('-')
-    state = int(get_one['data']['state'])
-    if time.localtime().tm_mday != int(date[-1]) or time.localtime().tm_mon != int(date[-2]) or time.localtime().tm_year!= int(date[0]):
-        print("已超过一天逾期！")
-        return False
-    if state == 4:
-        if not DelayBackTime(id , headers_one):
-            return False
-    if state == 5:
-        print("已手动返校！")
-        return True
-    req = requests.get(get_one_url, headers=headers_one)
-    get_second = json.loads(req.text)
-    state = get_second['data']['state']
-    id = get_second['data']['id']
-    headers_two = {
-        'Host': 'gw.wozaixiaoyuan.com',
-        'accept': 'application/json, text/plain, */*',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 8.1.0; OPPO R11st Build/OPM1.171019.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4425 MMWEBSDK/20230202 Mobile Safari/537.36 MMWEBID/4162 MicroMessenger/8.0.33.2320(0x28002137) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wxce6d08f781975d91',
-        'content-type': 'application/json;charset=UTF-8',
-        'x-requested-with': 'com.tencent.mm',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'referer': f'https://gw.wozaixiaoyuan.com/h5/mobile/out/index/out/back?id={id}',
-        'accept-encoding': 'gzip, deflate',
-        'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cookie': cookie
-    }
-    if state == 2:
-        back_url = 'https://gw.wozaixiaoyuan.com/out/mobile/out/back?id=' + id
-        req = requests.get(back_url, headers=headers_two)
-        text = json.loads(req.text)
-        if text['code'] == 0:
-            print("成功返校！")
-            return True
-        else:
-            print("返校失败！", text)
-            return False
-
-
-
 
 def main():
     for config in configs:
         username = config['username']
         headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; WLZ-AN00 Build/HUAWEIWLZ-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4343 MMWEBSDK/20220903 Mobile Safari/537.36 MMWEBID/4162 MicroMessenger/8.0.28.2240(0x28001C35) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wxce6d08f781975d91'}
-        cookie = GetEachJws(config, headers)
-        if cookie:
-            login_code = testLoginStatus(headers, cookie)
+        jws, wzxySession = GetEachJws(config, headers)
+        if jws:
+            login_code = testLoginStatus(headers, jws, wzxySession)
         else:
             continue
         if login_code is False:
             print(config['username'], "jws失效！")
-            cookie = Login(headers, config['username'], config['password'])
-            if cookie is False:
+            jws, wzxySession = Login(headers, config['username'], config['password'])
+            if jws is False:
                 continue
             print("jws文件更新成功")
-            jws_data[config['username']] = cookie
+            jws_data[config['username']] = f"{jws},{wzxySession}"
         headers = {
             'Host': 'gw.wozaixiaoyuan.com',
             'Connection': 'keep-alive',
             'Accept': 'application/json, text/plain, */*',
-            "Cookie": cookie,
+            'jwsession': jws,
+            "cookie": f'JWSESSION={jws}',
+            "cookie": f'JWSESSION={jws}',
+            "cookie": f'WZXYSESSION={wzxySession}',
             'User-Agent': 'Mozilla/5.0 (Linux; Android 10; WLZ-AN00 Build/HUAWEIWLZ-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4343 MMWEBSDK/20220903 Mobile Safari/537.36 MMWEBID/4162 MicroMessenger/8.0.28.2240(0x28001C35) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wxce6d08f781975d91',
             'Content-Type': 'application/json;charset=UTF-8',
             'X-Requested-With': 'com.tencent.mm',
@@ -297,12 +258,6 @@ def main():
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
         }
-        if config['back']:
-            if BackToSchool(headers , cookie):
-                print(username , "返校成功！")
-                mail.send(config['receive'] , '返校成功！' , '返校成功！')
-            else:
-                mail.send(config['receive'] , '返校失败！' , '返校失败！请查看！')
         batch = GetUnDo(headers, username)
         if not batch:
             continue
