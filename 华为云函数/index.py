d36bf38fadaf04e54c5dd397c4219b411cb4f9e1
lateyoung <lateyoung1@gmail.com>
2024-04-29 16:15:44 +0800

    删除华为云函数使用

--- 华为云函数/index.py
+++
@@ -1,9 +0,0 @@
-from WoZaiXiaoYuan import Start
-from GoOut import go_out
-from time import localtime
-
-def handler(event , context):
-    if localtime().tm_hour >= 20:
-        Start()
-    else:
-        go_out()
