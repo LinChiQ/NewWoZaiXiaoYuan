5cae29f6b9af3e08680c17215e61c7829a8e6893
Lin <111717457+LinChiQ@users.noreply.github.com>
2024-05-02 20:02:22 +0800

    Add files via upload

---
+++ actionVersion.py
@@ -0,0 +1,188 @@
+import requests
+import json
+import yagmail
+import re
+import os
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from base64 import b64encode
+
+
+def encrypt(t, e):
+    t = str(t)
+    key = e.encode('utf-8')
+    cipher = AES.new(key, AES.MODE_ECB)
+    padded_text = pad(t.encode('utf-8'), AES.block_size)
+    encrypted_text = cipher.encrypt(padded_text)
+    return b64encode(encrypted_text).decode('utf-8')
+
+
+def Login(headers, username, password):
+    headers00 = {
+        "accept": "application/json, text/plain, */*",
+        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/119.0.0.0"}
+    url00 = "https://gw.wozaixiaoyuan.com/basicinfo/mobile/login/getSchoolList"
+    response00 = requests.get(url00, headers=headers00)
+    school_data = json.loads(response00.text)['data']
+    def find_school_id(school_name, data):
+        for school in data:
+            if school['name'] == school_name:
+                return school['id']
+        return None
+    school = os.environ["school_name"]
+    school_id = find_school_id(school, school_data)
+    key = (str(username) + "0000000000000000")[:16]
+    encrypted_text = encrypt(password, key)
+    login_url = 'https://gw.wozaixiaoyuan.com/basicinfo/mobile/login/username'
+    params = {
+        "schoolId": school_id,
+        "username": username,
+        "password": encrypted_text
+    }
+    login_req = requests.post(login_url, params=params, headers=headers)
+    print(login_req.text)
+    text = json.loads(login_req.text)
+    if text['code'] == 0:
+        print(f"{username}账号登陆成功！")
+        set_cookie = login_req.headers['Set-Cookie']
+        jws = re.search(r'JWSESSION=(.*?);', str(set_cookie)).group(1)
+        wzxySession = re.search(r'WZXYSESSION=(.*?);',
+                                str(set_cookie)).group(1)
+        return jws, wzxySession
+    else:
+        print(f"{username}登陆失败，请检查账号密码！")
+        return False
+
+
+def testLoginStatus(headers, jws, wzxySession):
+    # 用任意需要鉴权的接口即可，这里随便选了一个
+    url = "https://gw.wozaixiaoyuan.com/health/mobile/health/getBatch"
+    headers['Host'] = "gw.wozaixiaoyuan.com"
+    headers['cookie'] = f'JWSESSION={jws}'
+    headers['cookie'] = f'JWSESSION={jws}'
+    headers['cookie'] = f'WZXYSESSION={wzxySession}'
+    res = requests.get(url, headers=headers)
+    text = json.loads(res.text)
+    if text['code'] == 0:
+        return True
+    elif text['code'] == 103:
+        return False
+    else:
+        return 0
+
+
+def GetUnDo(headers, username):
+    url = 'https://gw.wozaixiaoyuan.com/health/mobile/health/getBatch'
+    res = requests.get(url, headers=headers)
+    lists = json.loads(res.text)['data']
+    for list in lists['list']:
+        if list['state'] == 1 and list['type'] == 0:
+            return list['id']
+    print(f"{username}未找到未打卡项目！")
+    return False
+
+
+def GetAnswers(headers, username, batch):
+    answers = {}
+    url = 'https://gw.wozaixiaoyuan.com/health/mobile/health/getForm?batch='+batch
+    res = requests.get(url, headers=headers)
+    data = json.loads(res.text)
+    locationType = data['data']['locationType']
+    answers.update({"type": 0, "locationMode": 0,
+                   "locationType": locationType})
+    print(f"获取{username}的参数成功！")
+    return answers
+
+
+def GetLocation(config_locations):
+    location = config_locations['location']
+    locations = []
+    for _ in location:
+        if _ == '省' or _ == '市' or _ == '州' or _ == '区' or _ == '县' or _ == '岛' or _ == '域' or _ == '道' or _ == '路' or _ == '乡' or _ == '镇':
+            locations.append(location[:location.index(_) + 1])
+            location = location[location.index(_) + 1:]
+    locate = locations.copy()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'cache', 'location.json')
+    with open(file_path, 'r', encoding='utf-8') as f:
+        txt = json.loads(f.read())
+    datas = []
+    while len(locations) != 1:
+        for i in txt:
+            if i['label'] == locations[0]:
+                datas.append(i['value'])
+                locations.pop(0)
+                try:
+                    txt = i['children']
+                except KeyError:
+                    break
+    location = {"location": f"中国/{locate[0]}/{locate[1]}/{locate[2]}/{locate[3]}/{locate[4]}/156/{datas[-2]}/156{datas[1]}/{datas[-1]}/{config_locations['longitude']}/{config_locations['latitude']}"
+                }
+    return location
+
+
+def Punch(headers, batch, answers, username):
+    receive = os.environ['receive_mail']
+    url = 'https://gw.wozaixiaoyuan.com/health/mobile/health/save?batch='+batch
+    res = requests.post(url, json=answers, headers=headers)
+    txt = json.loads(res.text)
+    if txt['code'] == 0:
+        print(f"{username}打卡成功！\n")
+        mail.send(receive, "打卡成功！", "打卡成功！")
+        return True
+    else:
+        print(f"{username}打卡失败！{str(txt)}\n")
+        mail.send(receive, "打卡失败！", str(txt))
+        return False
+
+
+def ReturnMail():
+    mail = yagmail.SMTP(os.environ['mail_address'],
+                        os.environ['mail_password'], os.environ['mail_host'])
+    return mail
+
+
+def GetEachUser(username, headers, batch):
+    answers = GetAnswers(headers, username, batch)
+    location = GetLocation(os.environ['punch_location'])
+    answers.update(location)
+    return answers
+
+
+def main():
+    username = os.environ['wzxy_username']
+    login_headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 10; WLZ-AN00 Build/HUAWEIWLZ-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4343 MMWEBSDK/20220903 Mobile Safari/537.36 MMWEBID/4162 MicroMessenger/8.0.28.2240(0x28001C35) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wxce6d08f781975d91'}
+    jws, wzxySession = Login(login_headers, username,
+                             os.environ['wzxy_password'])
+    if testLoginStatus(headers, jws, wzxySession):
+        headers = {
+            'Host': 'gw.wozaixiaoyuan.com',
+            'Connection': 'keep-alive',
+            'Accept': 'application/json, text/plain, */*',
+            'jwsession': jws,
+            "cookie": f'JWSESSION={jws}',
+            "cookie": f'JWSESSION={jws}',
+            "cookie": f'WZXYSESSION={wzxySession}',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; WLZ-AN00 Build/HUAWEIWLZ-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/4343 MMWEBSDK/20220903 Mobile Safari/537.36 MMWEBID/4162 MicroMessenger/8.0.28.2240(0x28001C35) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wxce6d08f781975d91',
+            'Content-Type': 'application/json;charset=UTF-8',
+            'X-Requested-With': 'com.tencent.mm',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://gw.wozaixiaoyuan.com/h5/mobile/health/0.3.7/health',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
+        }
+        batch = GetUnDo(headers, username)
+        if not batch:
+            return False
+        answers = GetEachUser(username, headers, batch)
+        Punch(headers, batch, answers, username)
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    mail = ReturnMail()
+    main()
